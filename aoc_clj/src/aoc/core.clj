01f7061777a56562bb6191263b11df25ec47ae69
juan serrano <germgerm@gmail.com>
2018-12-03 18:14:57 -0800

    Initial clojure commit.

---
+++ aoc_clj/src/aoc/core.clj
@@ -0,0 +1,8 @@
+(ns aoc.core
+  (:gen-class)
+  (:use [aoc.eighteen.day01.solution01]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (aoc.eighteen.day01.solution01/execute))
---
+++ aoc_clj/src/aoc/eighteen/day01/solution01.clj
@@ -0,0 +1,8 @@
+(ns aoc.eighteen.day01.solution01)
+
+(def content
+  (slurp "src/aoc/eighteen/day01/input"))
+
+(defn execute []
+  (->> (slurp "src/aoc/eighteen/day01/input")
+       (println)))
